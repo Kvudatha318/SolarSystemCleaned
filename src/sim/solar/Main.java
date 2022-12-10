f31eea945d453324ce5373b8c32c61ca8752503e
Kvudatha318 <118829016+Kvudatha318@users.noreply.github.com>
2022-12-10 19:17:33 +0530

    added main class in a package

---
+++ src/sim/solar/Main.java
@@ -0,0 +1,8 @@
+package sim.solar;
+
+public class Main {
+  public static void main(String[] args)  {
+     sim.solar.DisplayFrame frame = new sim.solar.DisplayFrame();
+     frame.Activate();
+  }
+}
